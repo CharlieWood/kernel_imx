b2b2abfcab67fedde135a7960ed0216ae49d5aa8
Charlie Mu <charlie.wood.mu@gmail.com>
2011-09-27 17:07:38 +0800

    makefile: add Android.mk to build on android 3.0
    Change-Id: I12eaa86c245b170cb1fc9c1f4b31c170ff586d1e

---
+++ Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_KERNEL_CONFIG_NAME),)
+$(error "TARGET_KERNEL_CONFIG_NAME not set")
+endif
+
+kernelimage: KERNEL_ROOT := $(LOCAL_PATH)
+$(PRODUCT_OUT)/uImage: KERNEL_ROOT := $(LOCAL_PATH)
+clean-kernel: KERNEL_ROOT := $(LOCAL_PATH)
+
+.PHONY: FORCE
+$(LOCAL_PATH)/arch/arm/boot/uImage: FORCE
+	@echo "start build kernel with cfgfile: $(TARGET_KERNEL_CONFIG_NAME)"
+	+$(MAKE) -C "$(KERNEL_ROOT)" $(TARGET_KERNEL_CONFIG_NAME) \
+		ARCH=arm \
+		CROSS_COMPILE=${ANDROID_BUILD_TOP}/$(TARGET_TOOLS_PREFIX)
+	+$(MAKE) -C "$(KERNEL_ROOT)" uImage \
+		ARCH=arm \
+		CROSS_COMPILE=${ANDROID_BUILD_TOP}/$(TARGET_TOOLS_PREFIX)
+
+clean-kernel::
+	$(MAKE) -C "$(KERNEL_ROOT)" clean
+
+LOCAL_MODULE := kernelimage
+LOCAL_MODULE_STEM := uImage
+LOCAL_SRC_FILES := arch/arm/boot/uImage
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+LOCAL_MODULE_CLASS := ETC
+
+include $(BUILD_PREBUILT)
